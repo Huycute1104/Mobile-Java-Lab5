012f76ddb9dc9249331c6e9c7ceaf3ff1b172c60
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-07-08 15:22:09 +0700

    update

--- Lab5_RecycleView02/app/src/main/java/com/example/lab5_recycleview02/ModuleActivity.java
+++ Lab5_RecycleView02/app/src/main/java/com/example/lab5_recycleview02/ModuleActivity.java
@@ -24,9 +24,10 @@ public class ModuleActivity extends AppCompatActivity {
         RecyclerView rvModuleList = findViewById(R.id.rvModuleList);
 
         modules = new ArrayList<>();
-        modules.add(new Module("Android","ListView trong Android","trong Android là một thành phần giao diện người dùng (UI component) cho phép bạn hiển thị một danh sách các mục (items) theo dạng cuộn dọc (scrollable vertical list).",R.drawable.android));
-        modules.add(new Module("IOS", "Xử lý sự kiện trong IOS","Xử lý sự kiện trong iOS là một phần quan trọng trong phát triển ứng dụng iOS, giúp tương tác người dùng với giao diện ứng dụng. Các sự kiện có thể bao gồm chạm, vuốt, kéo, và các hành động khác. ", R.drawable.ios));
-
+//        modules.add(new Module("Android","ListView trong Android","trong Android là một thành phần giao diện người dùng (UI component) cho phép bạn hiển thị một danh sách các mục (items) theo dạng cuộn dọc (scrollable vertical list).",R.drawable.android));
+//        modules.add(new Module("IOS", "Xử lý sự kiện trong IOS","Xử lý sự kiện trong iOS là một phần quan trọng trong phát triển ứng dụng iOS, giúp tương tác người dùng với giao diện ứng dụng. Các sự kiện có thể bao gồm chạm, vuốt, kéo, và các hành động khác. ", R.drawable.ios));
+        modules.add(new Module("Android", "ListView trong Android", "ListView trong Android là một thành phần để xử lý nhóm nhiều mục (view)",R.drawable.and));
+        modules.add(new Module("IOS","Xử lý sự kiện trong IOS","Xử lý các sự kiện trong IOS. Sau khi các bạn đã biết cách thiết kế giao diện ứng dụng IOS", R.drawable.ios));
 
         rvModuleList.setAdapter(new ModuleAdapter(modules));
         rvModuleList.setLayoutManager(new LinearLayoutManager(this));
